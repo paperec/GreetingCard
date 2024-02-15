4a72e74167e0f86aa5dd1ef421bdbe56a7ae1348
paperec <paperec@unirioja.es>
2024-02-15 18:28:15 +0100

    Fin leccion 3 apartado 7

--- app/src/main/java/com/example/greetingcard/MainActivity.kt
+++ app/src/main/java/com/example/greetingcard/MainActivity.kt
@@ -12,6 +12,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.greetingcard.ui.theme.GreetingCardTheme
+import androidx.compose.ui.unit.dp;
+import androidx.compose.foundation.layout.padding;
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,7 +33,8 @@ class MainActivity : ComponentActivity() {
 fun Greeting(name: String/*, modifier: Modifier = Modifier*/) {
     Surface(color = Color.Magenta) {
         Text(
-            text = "Hi, my name is $name!"
+            text = "Hi, my name is $name!",
+            modifier = Modifier.padding(24.dp)
         )
     }
 }
