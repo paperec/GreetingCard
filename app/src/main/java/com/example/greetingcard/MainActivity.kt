1932ce513c7d738e5ea7104c071e42578f83a9fb
paperec <paperec@unirioja.es>
2024-02-15 18:31:19 +0100

    FIN CREAR PRIMERA APP

--- app/src/main/java/com/example/greetingcard/MainActivity.kt
+++ app/src/main/java/com/example/greetingcard/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -11,9 +12,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.greetingcard.ui.theme.GreetingCardTheme
-import androidx.compose.ui.unit.dp;
-import androidx.compose.foundation.layout.padding;
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
